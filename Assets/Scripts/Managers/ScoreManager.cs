a2c7ee04a2125b7d6bae23e910a11556f02e7da6
Nilesh <nilesh.navgekar@gmail.com>
2023-12-03 11:39:40 +0530

    added score manager

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using TMPro;
+public class ScoreManager : Singleton<ScoreManager>
+{
+    public TextMeshProUGUI scoreText; // Reference to the UI text to display the score
+    private float startTime;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("StartTime"))
+        {
+            float savedTime = PlayerPrefs.GetFloat("StartTime");
+            startTime = Time.time - savedTime; // Resume from the saved time
+        }
+        else
+        {
+            startTime = Time.time; // Record the start time when the maze begins
+            PlayerPrefs.SetFloat("StartTime", startTime); // Save the start time
+        }
+    }
+
+    // Call this method when a checkpoint is completed
+    public void OnCheckpointReached()
+    {
+        float elapsedTime = Time.time - startTime;
+        PlayerPrefs.SetFloat("ElapsedTime", elapsedTime);
+        PlayerPrefs.Save();
+    }
+
+    private void Update()
+    {
+        // Calculate the elapsed time since the maze started
+        float elapsedTime = Time.time - startTime;
+
+        // Display the elapsed time as the score
+        DisplayScore(elapsedTime);
+    }
+
+    private void DisplayScore(float time)
+    {
+        // Display the time as the score on the UI text
+        scoreText.text = "Score: " + Mathf.Round(time).ToString();
+    }
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T instance;
+
+    public static T Instance { get { return instance; } }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = (T)this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
+
